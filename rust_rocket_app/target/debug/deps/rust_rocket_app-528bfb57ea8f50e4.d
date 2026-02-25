a259dd6b0a00155a45cf7fe319ad20a1561f06f2
Poluux <hugoboss82001@hotmail.com>
2026-02-25 09:51:31 +0100

    Creation of rust cargo project

---
+++ rust_rocket_app/target/debug/deps/rust_rocket_app-528bfb57ea8f50e4.d
@@ -0,0 +1,5 @@
+C:\VSCode_Projects\63-51_Emerging-Technologies\RustProject\RustForTheWeb\rust_rocket_app\target\debug\deps\rust_rocket_app-528bfb57ea8f50e4.d: src\main.rs
+
+C:\VSCode_Projects\63-51_Emerging-Technologies\RustProject\RustForTheWeb\rust_rocket_app\target\debug\deps\librust_rocket_app-528bfb57ea8f50e4.rmeta: src\main.rs
+
+src\main.rs:
---
+++ rust_rocket_app/target/debug/deps/rust_rocket_app-9cd10dbd43af59b2.d
@@ -0,0 +1,5 @@
+C:\VSCode_Projects\63-51_Emerging-Technologies\RustProject\RustForTheWeb\rust_rocket_app\target\debug\deps\rust_rocket_app-9cd10dbd43af59b2.d: src\main.rs
+
+C:\VSCode_Projects\63-51_Emerging-Technologies\RustProject\RustForTheWeb\rust_rocket_app\target\debug\deps\librust_rocket_app-9cd10dbd43af59b2.rmeta: src\main.rs
+
+src\main.rs:
