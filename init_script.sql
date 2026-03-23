0b3b4c488640824fb39f2dec719c4d88adbdc8d5
Lord-of-Waffles <ben.worton@gmail.com>
2026-03-23 10:21:09 +0100

    added giant int array to postgresdb

--- init_script.sql
+++ init_script.sql
@@ -1,5 +1,6 @@
 \c postgres_db;
 
+-- code for making test users
 CREATE TABLE users (
     id SERIAL PRIMARY KEY,
     name VARCHAR(100),
@@ -10,3 +11,14 @@ INSERT INTO users (name, email) VALUES
     ('Benjamin', 'benjamin@example.com'),
     ('Hugo', 'hugo@example.com'),
     ('Matthias', 'matthias@example.com');
+
+-- code for making giant test array 
+CREATE TABLE sorting_test (
+    id serial PRIMARY KEY,
+    numbers integer[]
+);
+
+-- make the giant array
+INSERT INTO sorting_test (numbers)
+SELECT array_agg(floor(random() * 1000000)::int)
+FROM generate_series(1, 100000);
